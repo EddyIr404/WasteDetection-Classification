d3379d0acf5cf5ae627ca5e2a53c92aad985fee4
EddyIr404 <eddyirvine02@gmail.com>
2026-01-14 22:48:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from PIL import Image
 import numpy as np
 import torch
 import os
+from PIL import Image
 from huggingface_hub import hf_hub_download
 
 os.environ["YOLO_MISSING_CV2"] = "1"
@@ -24,10 +25,10 @@ st.write("Upload an image and the model will predict what type of waste it is.")
 
 uploaded_file = st.file_uploader("Upload an image", type=["jpg","jpeg","png"])
 
-if uploaded_file is not None:
-    # Display image
+if uploaded_file:
     img = Image.open(uploaded_file).convert("RGB")
-    st.image(img, caption="Uploaded Image", use_column_width=True)
+    img_np = np.array(img)  # YOLO accepts numpy arrays
+    results = model.predict(img_np, imgsz=256)
 
     # Run prediction
     results = model.predict(img, verbose=False)
